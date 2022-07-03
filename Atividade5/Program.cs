c5be92ad60bbdba6730160a35ec5c863f684ca71
Vinny Pereira Alves <vinny.alves004@hotmail.com>
2022-07-03 12:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador;
+
+            contador = 0;
+
+            while (contador < 100)
+            {
+                contador++; 
+                contador++;
+                Console.WriteLine(contador);
+            }
+;
+            Console.ReadKey();
+        }
+    }
+}
